d11c899ea2649588244185028cb5006dbd8578b8
0XRKX <alexberarg95@gmail.com>
2023-10-19 20:13:31 +0200

    SEO

--- app/erc20/page.jsx
+++ app/erc20/page.jsx
@@ -5,6 +5,32 @@ import App from './App'
 
 import { config } from '@/utils/WagmiConfig'
 
+export const metadata = {
+    title: 'ERC20',
+    description: 'Mint ERC20 Tokens on Sepolia Network',
+    openGraph: {
+        title: 'Next.js',
+        description: 'The React Framework for the Web',
+        url: 'https://nextjs.org',
+        siteName: 'Next.js',
+        images: [
+            {
+                url: 'https://nextjs.org/og.png',
+                width: 800,
+                height: 600,
+            },
+            {
+                url: 'https://nextjs.org/og-alt.png',
+                width: 1800,
+                height: 1600,
+                alt: 'My custom alt',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+}
+
 function ERC20() {
 
     return (
--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,27 @@ const vt323 = VT323({ subsets: ['latin'], weight: ['400'] })
 export const metadata = {
   title: 'Mint All ERC',
   description: 'Mint all the ERC in one website',
+  openGraph: {
+    title: 'Next.js',
+    description: 'Mint all the ERC in one website',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({ children }) {
